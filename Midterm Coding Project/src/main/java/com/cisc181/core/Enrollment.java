34a241b57b64e68db9811aae91b3bb1618dc4f01
Paul Jureidini <pauldeam@udel.edu>
2016-03-20 15:36:49 -0400

    yes

---
+++ Midterm Coding Project/src/main/java/com/cisc181/core/Enrollment.java
@@ -0,0 +1,26 @@
+package com.cisc181.core;
+
+import java.util.UUID;
+
+public class Enrollment {
+
+	UUID SectionID, StudentID, EnrollmentID;
+	double Grade;
+	
+	private void Enrollment()
+	{
+
+	}
+	
+	private UUID Enrollment(int StudentID, int SectionID)
+	{
+		return EnrollmentID;
+
+	}
+	
+	public void SetGrade(double grade)
+	{
+		
+	}
+	
+}
